1a67927249f50ed8f6f5226ccc20d9a603ecbf19
wang <jingli@qq.com>
2016-07-17 19:34:20 +0800

    搭建私人通讯录界面

---
+++ swift超级猜图/swift超级猜图/UserAddressVc.swift
@@ -0,0 +1,32 @@
+//
+//  UserAddressVc.swift
+//  swift超级猜图
+//
+//  Created by WZZ on 16/7/17.
+//  Copyright © 2016年 WZZ. All rights reserved.
+//
+
+import UIKit
+
+class UserAddressVc: UIViewController {
+    
+    @IBOutlet weak var userTextFilerld: UITextField!
+    
+    @IBOutlet weak var passwordFileld: UITextField!
+    
+    @IBOutlet weak var savePassWord: UISwitch!
+    
+    @IBOutlet weak var automicLogin: UISwitch!
+    
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    
+}
+
+extension UserAddressVc {
+    
+    @IBAction func loginBtnDidClick(sender: UIButton) {
+    }
+    
+    
+}
